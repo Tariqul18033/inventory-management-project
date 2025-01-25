36c53e527a18d6544a34281b8c8873dc93b7e793
Tariqul <tariqul18033@gmail.com>
2025-01-25 12:05:02 +0600

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+   function userRegistration(Request $request){
+
+    User::create([
+        'firstname'=>$request->input('firstname'),
+        'lastname'=>$request->input('lastname'), 
+        'email'=>$request->input('email'),
+        'phone'=>$request->input('phone'),
+        'password'=>$request->input('password'),
+    ]);
+
+
+   } 
 }
