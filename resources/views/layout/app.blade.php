b3c427c1687927ff528f85d97f3cebf41ce0a6e4
Tariqul <tariqul18033@gmail.com>
2025-01-27 10:23:30 +0600

    Aded nev

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,8 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sales Inventory Management</title>
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/animate.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/toastify.min.css')}}" rel="stylesheet" />
+    <script src="{{asset('js/toastify-js.js')}}"></script>
+    <script src="{{asset('js/axios.min.js')}}"></script>
+    <script src="{{asset('js/config.js')}}"></script>
 </head>
+
 <body>
-    
+
+<div id="loader" class="LoadingOverlay d-none">
+<div class="Line-Progress">
+    <div class="indeterminate"></div>
+</div>
+</div>
+
+<div>
+    @yield('content')
+</div>
+<script>
+
+</script>
+
+<script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+
 </body>
-</html>
+</html
